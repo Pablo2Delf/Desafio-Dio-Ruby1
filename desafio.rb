4ca8244eabace3df64b75424eba0b3a071fdbc61
Pablo Delfino <pabloxdelfino@gmail.com>
2024-02-18 23:49:46 -0300

    finalizado
    finalizado

---
+++ desafio.rb
@@ -0,0 +1,10 @@
+puts "Digite seu nome: "
+nome = gets.chomp
+
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+puts "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá #{nome} #{sobrenome}. Idade: #{idade}."
